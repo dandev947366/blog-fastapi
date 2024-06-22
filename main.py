6d15e833ae896c009b9b608557e20c3b4aaa8520
dandev947366 <dan.dev947366@gmail.com>
2024-06-22 22:16:33 +0300

    add validation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path, status
 from typing import Annotated
 from sqlalchemy.orm import Session
 import models
@@ -16,6 +16,15 @@ def get_db():
     finally:
         db.close() 
         
-@app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
     return db.query(Blogs).all()
+    
+@app.get("/blogs/{blog_id}", status_code=status.HTTP_200_OK)
+async def read_blog(db: Session = Depends(get_db), blog_id: int = Path(..., gt=0)):
+    blog_model = db.query(Blogs).filter(Blogs.id == blog_id).first()
+    if blog_model:
+        return blog_model
+    raise HTTPException(status_code=404, detail="Blog not found")

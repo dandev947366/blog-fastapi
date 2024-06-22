d9a5de92ba1d3be30d1c0f17b1eaeb6afcb763f8
dandev947366 <dan.dev947366@gmail.com>
2024-06-23 00:01:48 +0300

    add Users Model

--- models.py
+++ models.py
@@ -1,6 +1,16 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import ForeignKey, Column, Integer, String, Boolean
 
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 class Blogs(Base):
     __tablename__ = 'blogs'
     id = Column(Integer, primary_key=True, index=True)
@@ -8,4 +18,4 @@ class Blogs(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
-    
+    owner_id = Column(Integer, ForeignKey("users.id"))

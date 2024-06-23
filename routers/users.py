8399d67285538a05fb40581aaa9005f8543191a0
dandev947366 <dan.dev947366@gmail.com>
2024-06-23 17:09:40 +0300

    done 🫠

--- routers/users.py
+++ routers/users.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from models import Blogs, Users
 from database import SessionLocal
 from .auth import get_current_user
-
+from passlib.context import CryptContext
 router = APIRouter()
 router = APIRouter(
     prefix="/user",
@@ -20,12 +20,31 @@ def get_db():
         
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
-    
-@router.get("/blogs/{user_id}", status_code=status.HTTP_200_OK)
-async def read_user_info(user: user_dependency, db: db_dependency, user_id: int = Path(..., gt=0)):
-    if user is None:
+bcrypt_context = CryptContext(schemes=['bcrypt'],deprecated='auto')
+
+class UserVerification(BaseModel):
+    password:str
+    new_password: str=Field(min_length=6)
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def read_user_info(user: user_dependency, db: db_dependency):
+    if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication failed")
-    user_model = db.query(Blogs).filter(Users.id == user_id).filter(Users.id==user.get('id')).first()
+    
+    user_model = db.query(Users).filter(Users.id == user['id']).first()
     if user_model:
         return user_model
-    raise HTTPException(status_code=404, detail="User not found")
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    
+@router.put("/password",status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependency, db: db_dependency, user_verification:UserVerification):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication failed")
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Error on password change")
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
